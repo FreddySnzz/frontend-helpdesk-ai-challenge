381d424ec090026f641666bdf1a6a5c5938f51f7
FreddySnzz <fredsonluiz.03@gmail.com>
2026-08-14 14:08:22 -0300

    feat: initial config

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-sans bg-primary text-secondary">
+      <main className="flex flex-1 w-full">
+        Hello world
+      </main>
+    </div>
+  );
+}
